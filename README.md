e6eff47a595fcf3ba563084db4fadcfb3c2fee39
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-03-17 22:23:39 +0100

    First commit - Tutorial building API

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BikenAPI
 REST-API in Python for Biken
+
+Currently : 
+- Testing the example here : https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
---
+++ api/api.py
@@ -0,0 +1,44 @@
+import flask
+from flask import request, jsonify
+import data
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/api/v1/resources/books/all',methods=['GET'])
+def api_all():
+    return jsonify(data.books)
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in data.books:
+        if book['id'] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+app.run()
+
+
+app.run()
