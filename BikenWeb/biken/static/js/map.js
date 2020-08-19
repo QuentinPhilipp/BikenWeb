af0d7d783a450a00e6972c01138438e2f6b197b4
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-08-19 11:06:26 +0200

    Add fitItinerary methods

--- BikenWeb/biken/static/js/map.js
+++ BikenWeb/biken/static/js/map.js
@@ -94,6 +94,8 @@ function sendRequest() {
         // display the new route
         displayRoute(dataItinerary.data.waypoints);
 
+
+
         showSummary(dataItinerary);
 
       });
@@ -109,13 +111,15 @@ function sendRequest() {
         });
 
         // Display start point
-        displayStart(dataItinerary.data.waypoints[0])
+        startPoint = {lat : dataItinerary.data.waypoints[0][0],lon : dataItinerary.data.waypoints[0][1]}
+        displayStart(startPoint)
         // display the new route
         displayRoute(dataItinerary.data.waypoints);
 
         showSummary(dataItinerary);
 
-
+        // Fit the itinerary in the screen
+        fitItinerary(dataItinerary.data.waypoints);
 
       });
     }
@@ -237,10 +241,9 @@ function displayStartFinish(pointList) {
 
 
 function displayStart(point) {
-  var marker = L.marker([point[0], point[1]]).addTo(mymap);
+  var marker = L.marker([point.lat, point.lon]).addTo(mymap);
   routeList.push(marker);
-  var zoomPoint = L.latLng(point[0], point[1]);
-  mymap.flyTo(zoomPoint);
+
 }
 
 function addKmToLatitude(originalLat,kmToAdd) {
@@ -251,3 +254,95 @@ function addKmToLongitude(originalLat,originalLon,kmToAdd) {
     var r_earth = 6378;
     return originalLon + (kmToAdd / r_earth) * (180 / Math.PI) / Math.cos(originalLat *Math.PI/180);
   }
+
+
+
+function fitItinerary(waypoints) {
+  //Fit an itinerary on the screen
+  console.log("Fit",waypoints);
+
+
+
+  // Get 6 points splited on the itinerary
+
+  var waypointsLen = waypoints.length;
+
+  console.log("waypoints :",waypointsLen);
+
+  var coords0 = waypoints[0];
+  var coords1 = waypoints[parseInt(waypointsLen/6)];
+  var coords2 = waypoints[parseInt(2*waypointsLen/6)];
+  var coords3 = waypoints[parseInt(3*waypointsLen/6)];
+  var coords4 = waypoints[parseInt(4*waypointsLen/6)];
+  var coords5 = waypoints[parseInt(5*waypointsLen/6)];
+
+
+  // var marker0 = L.marker([coords0[0], coords0[1]]).addTo(mymap);
+  // var marker1 = L.marker([coords1[0], coords1[1]]).addTo(mymap);
+  // var marker2 = L.marker([coords2[0], coords2[1]]).addTo(mymap);
+  // var marker3 = L.marker([coords3[0], coords3[1]]).addTo(mymap);
+  // var marker4 = L.marker([coords4[0], coords4[1]]).addTo(mymap);
+  // var marker5 = L.marker([coords5[0], coords5[1]]).addTo(mymap);
+
+
+  //get the max distance between all the coords
+  var distance1 = distance(coords0[0],coords0[1],coords3[0],coords3[1]);
+  var distance2 = distance(coords1[0],coords1[1],coords4[0],coords4[1]);
+  var distance3 = distance(coords2[0],coords2[1],coords5[0],coords5[1]);
+
+  console.log("Distance 1:",distance1);
+  console.log("Distance 2:",distance2);
+  console.log("Distance 3:",distance3);
+
+
+  if (distance1>distance2 && distance1>distance3) {
+    var corner1 = L.latLng(coords0[0], coords0[1]);
+    var corner2 = L.latLng(coords3[0],coords3[1]);
+  }
+  else if (distance2>distance1 && distance2>distance3) {
+    var corner1 = L.latLng(coords1[0],coords1[1]);
+    var corner2 = L.latLng(coords4[0],coords4[1]);
+  }
+  else {
+    var corner1 = L.latLng(coords2[0],coords2[1]);
+    var corner2 = L.latLng(coords5[0],coords5[1]);
+  }
+
+
+  var bounds = L.latLngBounds(corner1, corner2);
+
+  // Padding to show the itiinerary even with the infos displayed
+  mymap.flyToBounds(bounds,{
+        animate: true,
+        duration: 1,
+        padding: [90,90]
+});
+
+  // var corner1 = L.latLng(pointList[0].lat, pointList[0].lon);
+  // var corner2 = L.latLng(pointList[1].lat, pointList[1].lon);
+  // var bounds = L.latLngBounds(corner1, corner2);
+
+}
+
+
+function distance(lat1, lon1, lat2, lon2) {
+  // https://www.geodatasource.com/developers/javascript
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		dist = dist * 1.609344
+		return dist;
+	}
+}
