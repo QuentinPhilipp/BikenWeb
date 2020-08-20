f127b94a31ddc4e1a2a85cd4f46eb52f3d98cb5e
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-08-21 00:18:29 +0200

    Login with Google button

--- BikenWeb/run.py
+++ BikenWeb/run.py
@@ -4,4 +4,4 @@ from biken import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',ssl_context="adhoc")
+    app.run(host='0.0.0.0',ssl_context="adhoc",debug=True)
