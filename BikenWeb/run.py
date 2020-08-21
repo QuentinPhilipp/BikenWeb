ce10268275e4d8d6a9a7c642adb573ef94cc517a
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-08-21 14:39:08 +0200

    Login Google

--- BikenWeb/run.py
+++ BikenWeb/run.py
@@ -1,7 +1,17 @@
 """Application entry point."""
 from biken import create_app
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',ssl_context="adhoc",debug=True)
+    if os.environ.get("ENV", None) == "DEV":
+        print("Start in dev mode")
+        app.run(host='0.0.0.0',debug=True,ssl_context="adhoc")
+
+    if os.environ.get("ENV", None) == "LIVE":
+        print("Start in dev mode")
+        app.run(host='0.0.0.0',debug=False)
