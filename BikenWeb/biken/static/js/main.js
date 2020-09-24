b6f3826d3b82880c02bbd51e5a40f73b624c554d
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-09-24 10:53:57 +0200

    Minor fix for sharing popup

--- BikenWeb/biken/static/js/main.js
+++ BikenWeb/biken/static/js/main.js
@@ -306,14 +306,14 @@ span.onclick = function() {
 
 function copyOnClick() {
   /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
+  var copyText = document.getElementById("urlCopyInput");
   /* Select the text field */
   copyText.select();
   copyText.setSelectionRange(0, 99999); /*For mobile devices*/
 
   /* Copy the text inside the text field */
   document.execCommand("copy");
-}
+  var copyButton = document.getElementById("copyButton");
+  copyButton.innerHTML="Copied!"
 
-showSaveExportPopup("iHJKTsbm2Dk9aZi4")
+}
