6a035ec8225748e85d746ef1d440b31dc84ee3fb
QuentinPhilipp <quentin.philipp@hotmail.fr>
2020-11-06 15:36:50 +0100

    Rework front+end

--- BikenWeb/biken/static/js/map.js
+++ BikenWeb/biken/static/js/map.js
@@ -1,152 +1,150 @@
-
-var mymap = L.map('mapid',{ zoomControl: false }).setView([48.8589507,2.2770202], 10);
+var mymap = L.map("mapid", { zoomControl: false }).setView(
+  [48.8589507, 2.2770202],
+  10
+);
 debugState = false;
 routeList = [];
 displayedPolyline = "";
 currentDistance = 0;
 currentDuration = 0;
 
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: "mapbox/streets-v11",
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYmlrZW5kZXYiLCJhIjoiY2tndXZ6ZG1sMHo5ODJ6cDlmZjFoMWdheSJ9.Z8rE4hBJxztTXy5EX5mMUw'
-}).addTo(mymap);
-
-
+    accessToken:
+      "pk.eyJ1IjoiYmlrZW5kZXYiLCJhIjoiY2tndXZ6ZG1sMHo5ODJ6cDlmZjFoMWdheSJ9.Z8rE4hBJxztTXy5EX5mMUw",
+  }
+).addTo(mymap);
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setPosition);
   } else {
-    print("User didn't allowed geolocation")
+    print("User didn't allowed geolocation");
   }
 }
 
 function setPosition(position) {
   var pos = L.latLng(position.coords.latitude, position.coords.longitude);
-  mymap.setView(pos,10);
+  mymap.setView(pos, 10);
 }
 
-getLocation()
+getLocation();
 
-
-function createChart(data)
-{
-  document.getElementById("elevation-container").hidden=false;
+function createChart(data) {
+  document.getElementById("elevation-container").hidden = false;
   new Chart(document.getElementById("elevationChart"), {
-    type: 'line',
+    type: "line",
     data: {
       labels: data.profile,
-      datasets: [{
+      datasets: [
+        {
           data: data.profile,
           borderColor: "#3e95cd",
-          fill: false
-        }
-      ]
+          fill: false,
+        },
+      ],
     },
     options: {
       maintainAspectRatio: false,
       scales: {
-              xAxes: [{
-                  ticks: {
-                      display: false //this will remove the label
-                  },
-                  gridLines: {
-                    display: false
-                  }
-              }],
-              yAxes: [{
-                  gridLines: {
-                    display: false
-                  }
-              }]
+        xAxes: [
+          {
+            ticks: {
+              display: false, //this will remove the label
+            },
+            gridLines: {
+              display: false,
+            },
           },
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+      },
       legend: {
-          display:false
+        display: false,
       },
       elements: {
-                point:{
-                    radius: 0
-                }
-            }
-    }
+        point: {
+          radius: 0,
+        },
+      },
+    },
   });
 }
 
+async function getElevation(itinerary) {
+  var alertContainer = document.getElementById("alertContainer");
 
-async function getElevation(itinerary)
-{
-  var alertContainer = document.getElementById('alertContainer');
-
-  $.post( "/routing/elevation", {
-      waypoints: JSON.stringify(itinerary)
-  },
-  function(data, status){
-        console.log(data);
-        if (data.profile.length > 0) {
-          createChart(data);
-        }
-        else {
-          alertContainer.innerHTML ='<div class="alert warning">This itinerary is too long to get the elevation</div>';
-          setTimeout(function(){
-              alertContainer.innerHTML = '';
-          }, 2000);
-        }
-    });
-
+  $.post(
+    "/routing/elevation",
+    {
+      waypoints: JSON.stringify(itinerary),
+    },
+    function (data, status) {
+      console.log(data);
+      if (data.profile.length > 0) {
+        createChart(data);
+      } else {
+        alertContainer.innerHTML =
+          '<div class="alert warning">This itinerary is too long to get the elevation</div>';
+        setTimeout(function () {
+          alertContainer.innerHTML = "";
+        }, 2000);
+      }
+    }
+  );
 }
 
-
-
-
-
-function displayPolyline(polylineData){
+function displayPolyline(polylineData) {
   var polyline = L.Polyline.fromEncoded(polylineData);
   polyline.setStyle({
-      color: '#F67C5A'
+    color: "#F67C5A",
   });
   polyline.addTo(mymap);
   routeList.push(polyline);
 }
-
 
 function displayMarker(point) {
   var marker = L.marker(point).addTo(mymap);
   routeList.push(marker);
 }
 
-
-
-
 function renderItinerary(itinerary) {
-    displayPolyline(itinerary.polyline);
+  displayPolyline(itinerary.polyline);
 
-    var coordinates = L.Polyline.fromEncoded(itinerary.polyline).getLatLngs();
-    if(itinerary["type"]=="oneway") {
-        var start = L.latLng(coordinates[0].lat, coordinates[0].lng);
-        var end = L.latLng(coordinates[coordinates.length-1].lat, coordinates[coordinates.length-1].lng);
-        
-        displayMarker(start);
-        displayMarker(end);    
-        
-        var bounds = L.latLngBounds(start, end);
-        
-        mymap.flyToBounds(bounds,{
-            animate: true,
-            duration: 1,
-            padding: [90,90]
-        });
+  var coordinates = L.Polyline.fromEncoded(itinerary.polyline).getLatLngs();
+  if (itinerary["type"] == "oneway") {
+    var start = L.latLng(coordinates[0].lat, coordinates[0].lng);
+    var end = L.latLng(
+      coordinates[coordinates.length - 1].lat,
+      coordinates[coordinates.length - 1].lng
+    );
 
-        // getElevation(dataItinerary.polyline);
+    displayMarker(start);
+    displayMarker(end);
 
-    }
-    else {
-        displayMarker(coordinates[0]); 
-    }
+    var bounds = L.latLngBounds(start, end);
+
+    mymap.flyToBounds(bounds, {
+      animate: true,
+      duration: 1,
+      padding: [90, 90],
+    });
+
+    // getElevation(dataItinerary.polyline);
+  } else {
+    displayMarker(coordinates[0]);
+  }
 }
-
-
--- BikenWeb/biken/static/js/profile.js
+++ BikenWeb/biken/static/js/profile.js
@@ -1,47 +1,44 @@
-
 function editName(itineraryID) {
   // Replace the name div with an input field
 
   // If click on the edit button to confirm
   if ($("#new-title").value != "") {
-    console.log("Validate with the button")
+    console.log("Validate with the button");
     $("#name-form").submit();
   }
 
-
-  var strID = "#activityName"+itineraryID;
-  var url = "itineraries?itinerary="+itineraryID;
-  $( strID ).replaceWith( '<form id="name-form" method="POST" action='+url+'><input id="new-title" class="submit_on_enter" type="text" name="name" placeholder="New name"></form>' );
+  var strID = "#activityName" + itineraryID;
+  var url = "itineraries?itinerary=" + itineraryID;
+  $(strID).replaceWith(
+    '<form id="name-form" method="POST" action=' +
+      url +
+      '><input id="new-title" class="submit_on_enter" type="text" name="name" placeholder="New name"></form>'
+  );
 
   $("#new-title").focus();
 
   //set the name
 }
 
-
-
 function deleteItinerary(itineraryID) {
   $.ajax({
-  url: 'itineraries?itinerary='+itineraryID,
-  method: 'DELETE',
-  success: function(response) {
-     location.reload();
-   }
- });
+    url: "itineraries?itinerary=" + itineraryID,
+    method: "DELETE",
+    success: function (response) {
+      location.reload();
+    },
+  });
 }
-
-
 
 function strToWaypoints(itineraryString) {
   // transform a string of coords to a list of waypoints
   var extractRegex = /((.+?),(.+?);)/g;
   var waypointsList = [];
   var result;
-  while((result = extractRegex.exec(itineraryString)) !== null) {
-      var coord = [result[2],result[3]];
-      waypointsList.push(coord);
+  while ((result = extractRegex.exec(itineraryString)) !== null) {
+    var coord = [result[2], result[3]];
+    waypointsList.push(coord);
   }
 
-  return waypointsList
-
+  return waypointsList;
 }
